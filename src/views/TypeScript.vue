d034a290a3070a22ff198ee2e5aa94d8eb56cbbb
yxy <1144975609@qq.com>
2025-04-11 13:51:49 +0800

    ts

--- src/views/TypeScript.vue
+++ src/views/TypeScript.vue
@@ -2,11 +2,42 @@
   <div>
     <h1>TypeScript 学习</h1>
     <hr />
+    <h3>1. TypeScript 基础类型</h3>
   </div>
 </template>
 <script lang="ts">
 export default {
-  setup() {},
+  setup() {
+    let name: string = 'Alice'
+    let age: number = 18
+    let isActive: boolean = true
+    let nothing: null = null
+    let notDefined: undefined = undefined
+
+    return {
+      name,
+      msg: 'Hello TypeScript',
+    }
+  },
+
+  mounted() {
+    console.log('TypeScript mounted')
+  },
+  methods: {
+    test() {
+      console.log('TypeScript test')
+    },
+  },
+  computed: {
+    testComputed() {
+      return 'TypeScript testComputed'
+    },
+  },
+  watch: {
+    testWatch(newVal: string, oldVal: string) {
+      console.log('TypeScript testWatch', newVal, oldVal)
+    },
+  },
 }
 </script>
 <style></style>
